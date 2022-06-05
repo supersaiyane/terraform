3841daf3f3c2278d7330b759d94e5bbe2ab8f746
supersaiyane <gurpreet.singh_89@outlook.com>
2022-06-05 12:03:04 +0530

    completed aws vpc

---
+++ class6-aws-vpc/v2-vpc-module-standarized/c2-genericvariables.tf
@@ -0,0 +1,20 @@
+#Input variables
+#Aws Variable
+variable "aws_region" {
+    description="Region in which Aws resource is to be created"
+    type = string
+    default = "ap-southeast-2"
+}
+
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "dev"
+}
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "SAP"
+}
